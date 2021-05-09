4d388ac9b69c504a2ec51c637b3035b92ee1ae27
duckbuddyy <emirhansoylu98@gmail.com>
2021-05-10 00:58:19 +0300

    binding.viewmodel = homeViewModelNavigation Component

---
+++ app/src/main/java/com/duckbuddyy/movtime/view/DetailFragment.kt
@@ -0,0 +1,33 @@
+package com.duckbuddyy.movtime.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.duckbuddyy.movtime.viewmodel.DetailViewModel
+import com.duckbuddyy.movtime.R
+
+class DetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DetailFragment()
+    }
+
+    private lateinit var viewModel: DetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/duckbuddyy/movtime/view/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.duckbuddyy.movtime.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.duckbuddyy.movtime.R
+import com.duckbuddyy.movtime.databinding.FragmentHomeBinding
+import com.duckbuddyy.movtime.viewmodel.HomeViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class HomeFragment : Fragment() {
+    private val homeViewModel: HomeViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentHomeBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.viewmodel = homeViewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+}
